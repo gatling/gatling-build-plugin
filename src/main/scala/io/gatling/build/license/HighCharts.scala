32cf48e689ff0d970f7e25a25be0541d2e4d14d5
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2015-04-21 15:23:19 +0200

    Fix license path

--- src/main/scala/io/gatling/build/license/HighCharts.scala
+++ src/main/scala/io/gatling/build/license/HighCharts.scala
@@ -5,7 +5,7 @@ import java.net.URL
 object HighCharts extends License {
 
   override val name = "Gatling Highcharts License"
-  override val url = new URL("https://raw.github.com/gatling/gatling-highcharts/master/src/main/resources/META-INF/LICENSE")
+  override val url = new URL("https://raw.githubusercontent.com/gatling/gatling-highcharts/master/LICENSE")
   override val headers = Map("scala" -> scalaHeader)
 
   def scalaHeader = (javaStyleBlockComment,
