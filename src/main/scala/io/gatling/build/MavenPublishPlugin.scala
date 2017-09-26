bff87c0285746256866cedc651350a3136ae0747
Stephane Landelle <slandelle@gatling.io>
2017-09-26 11:03:28 +0200

    Clean up private repo resolution
    Motivation:
    
    The current way to resolve private publish repo is a mess:
    * decide based on `pushToPrivateNexus` sbt settings to publish on
    public or private
    * populate `publishTo` settings based on envar
    * populate `credentials` settings based on properties file
    
    Configuring the env var every time we want to publish is a pita.
    The host value is duplicated in the envar and the props file. We could
    just use the props file, the only missing information is the scheme.
    
    Modification:
    
    * Drop envar
    * Add scheme in props file
    * Resolve `publishTo` settings based on props file
    
    Result:
    
    Easier publish process (no more envar)

--- src/main/scala/io/gatling/build/MavenPublishPlugin.scala
+++ src/main/scala/io/gatling/build/MavenPublishPlugin.scala
@@ -1,15 +1,44 @@
 package io.gatling.build
 
+import java.io.FileInputStream
+import java.util.Properties
+
+import scala.collection.JavaConverters._
 import scala.util.Properties._
 
 import sbt._
 import sbt.Keys._
 
 object MavenPublishKeys {
-  private val PrivateNexusRoot = sys.env.get("PRIVATE_NEXUS_BASE_URL")
-  private[build] val PrivateNexusReleases = PrivateNexusRoot.map("Private Nexus Releases" at _ + "content/repositories/releases/")
-  private[build] val PrivateNexusSnapshots = PrivateNexusRoot.map("Private Nexus Snapshots" at _ + "content/repositories/snapshots/")
-  private[build] def privateNexusRepository(version: String) = if (version.endsWith("-SNAPSHOT")) PrivateNexusSnapshots else PrivateNexusReleases
+
+  private[build] val SbtHome = Path.userHome / ".sbt"
+  private[build] val PrivateNexusCredentialsFile = SbtHome / ".private-nexus-credentials"
+
+  private val PrivateNexusRepositoriesRoot: Option[String] = {
+    if (PrivateNexusCredentialsFile.exists) {
+      val is = new FileInputStream(PrivateNexusCredentialsFile)
+      val credProps: Map[String, String] =
+        try {
+          val props = new Properties
+          props.load(is)
+          props.asScala.map { case (k, v) => (k.toString, v.toString.trim) }.toMap
+        } finally {
+          is.close()
+        }
+
+      for {
+        scheme <- credProps.get("scheme")
+        host <- credProps.get("host")
+      } yield s"$scheme://$host/content/repositories"
+
+    } else {
+      None
+    }
+  }
+
+  private[build] val PrivateNexusReleases: Option[MavenRepository] = PrivateNexusRepositoriesRoot.map("Private Nexus Releases" at _ + "/releases/")
+  private[build] val PrivateNexusSnapshots: Option[MavenRepository] = PrivateNexusRepositoriesRoot.map("Private Nexus Snapshots" at _ + "/snapshots/")
+  private[build] def privateNexusRepository(version: String): Option[MavenRepository] = if (version.endsWith("-SNAPSHOT")) PrivateNexusSnapshots else PrivateNexusReleases
 
   val githubPath = settingKey[String]("Project path on Github")
   val projectDevelopers = settingKey[Seq[GatlingDeveloper]]("List of contributors for this project")
@@ -35,7 +64,7 @@ object MavenPublishPlugin extends AutoPlugin {
     publishTo := (if (pushToPrivateNexus.value) privateNexusRepository(version.value) else publishTo.value),
     pomExtra := mavenScmBlock(githubPath.value) ++ developersXml(projectDevelopers.value),
     resolvers ++= (if (useSonatypeRepositories.value) sonatypeRepositories else Seq.empty) :+ Resolver.mavenLocal,
-    credentials += Credentials(Path.userHome / ".sbt" / (if (pushToPrivateNexus.value) ".private-nexus-credentials" else ".credentials"))
+    credentials += Credentials(if (pushToPrivateNexus.value) PrivateNexusCredentialsFile else SbtHome / ".credentials")
   )
 
   private def sonatypeRepositories: Seq[Resolver] = Seq(
