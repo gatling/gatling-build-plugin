e7ad2b13afbd92ecd52b58c141329dfa02c656e0
Pierre Dal-Pra <pdalpra@gatling.io>
2020-05-13 10:16:57 +0200

    Integrate scalafix and sort-imports

--- build.sbt
+++ build.sbt
@@ -20,8 +20,9 @@ scalacOptions := Seq(
 // -- DEPENDENCIES -- //
 // ------------------ //
 
-addSbtPlugin("org.scalameta"     % "sbt-scalafmt"         % "2.3.2")
-addSbtPlugin("com.github.gseitz" % "sbt-release"          % "1.0.11")
-addSbtPlugin("com.jsuereth"      % "sbt-pgp"              % "2.0.0")
-addSbtPlugin("de.heikoseeberger" % "sbt-header"           % "5.4.0")
-addSbtPlugin("org.xerial.sbt"    % "sbt-sonatype"         % "2.0")
+addSbtPlugin("ch.epfl.scala"     % "sbt-scalafix" % "0.9.15")
+addSbtPlugin("org.scalameta"     % "sbt-scalafmt" % "2.3.2")
+addSbtPlugin("com.github.gseitz" % "sbt-release"  % "1.0.11")
+addSbtPlugin("com.jsuereth"      % "sbt-pgp"      % "2.0.0")
+addSbtPlugin("de.heikoseeberger" % "sbt-header"   % "5.4.0")
+addSbtPlugin("org.xerial.sbt"    % "sbt-sonatype" % "2.0")
--- src/main/scala/io/gatling/build/BaseSettingsPlugin.scala
+++ src/main/scala/io/gatling/build/BaseSettingsPlugin.scala
@@ -3,11 +3,16 @@ package io.gatling.build
 import sbt.Keys._
 import sbt._
 import org.scalafmt.sbt.ScalafmtPlugin.autoImport.scalafmtOnCompile
+import scalafix.sbt.ScalafixPlugin.autoImport.{ scalafix, scalafixDependencies }
 
 object BaseSettingsPlugin extends AutoPlugin {
 
+  val scalafixCheck = taskKey[Unit]("Check that scalafix rules have been applied")
+
   override def requires = plugins.JvmPlugin
+
   override def trigger = allRequirements
+
   override def projectSettings = Seq(
     homepage := Some(url("https://gatling.io")),
     organization := "io.gatling",
@@ -37,8 +42,16 @@ object BaseSettingsPlugin extends AutoPlugin {
       "-language:postfixOps",
       "-Xfuture",
       "-target:jvm-1.8"
-    )
-  )
+    ),
+    scalafixDependencies += "com.nequissimus" %% "sort-imports" % "0.5.0",
+    (compile in Compile) := (compile in Compile).dependsOn((scalafix in Compile).toTask("")).value,
+    compile in Test := (compile in Test).dependsOn((scalafix in Test).toTask("")).value,
+  ) ++ scalafixCheckSettings
+
+  private val scalafixCheckSettings = {
+    val scalafixCheckTask = (scalafixCheck := scalafix.toTask(" --check").value)
+    inConfig(Compile)(scalafixCheckTask) ++ inConfig(Test)(scalafixCheckTask)
+  }
 
   private def configureUpdateOptions(options: UpdateOptions): UpdateOptions =
     options
