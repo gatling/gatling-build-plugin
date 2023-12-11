9974076424eb99574e6a01cf7a92ea9bd2532c4d
Sébastien BREVET <sbrevet@gatling.io>
2023-12-11 22:40:28 +0100

    chore: inception
    Motivation:
    We don't want to duplicate the list of plugins dependencies or the
    dependencies override
    
    Modifications:
     * Use the meta-build pattern to be able to use same sbt source for different
    level
    
    Result:
    Only one list of dependencies to manage

---
+++ project/project/PluginDependencies.scala
@@ -0,0 +1,23 @@
+import sbt.*
+import sbt.Keys.*
+
+object PluginDependencies {
+  val deps = Seq(
+    // ------------------ //
+    // -- DEPENDENCIES -- //
+    // ------------------ //
+    addSbtPlugin("org.scalameta"     % "sbt-scalafmt" % "2.5.2"),
+    addSbtPlugin("ch.epfl.scala"     % "sbt-scalafix" % "0.11.1"),
+    addSbtPlugin("com.github.sbt"    % "sbt-release"  % "1.1.0"),
+    addSbtPlugin("com.github.sbt"    % "sbt-pgp"      % "2.2.1"),
+    addSbtPlugin("de.heikoseeberger" % "sbt-header"   % "5.10.0"),
+    addSbtPlugin("org.xerial.sbt"    % "sbt-sonatype" % "3.10.0"),
+    addSbtPlugin("com.github.sbt"    % "sbt-dynver"   % "5.0.1"),
+    dependencyOverrides ++= Seq(
+      "org.eclipse.jgit"          % "org.eclipse.jgit" % "6.8.0.202311291450-r", // sbt-scalafix
+      "ch.qos.logback"            % "logback-core"     % "1.4.7", // sbt-sonatype
+      "com.google.guava"          % "guava"            % "23.0", // sbt-sonatype
+      "org.apache.httpcomponents" % "httpclient"       % "4.5.14" // sbt-sonatype
+    )
+  )
+}
