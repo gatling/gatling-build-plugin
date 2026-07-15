dfaff297fb87639f83077c896b1069973c01e429
Sébastien BREVET <sbrevet@gatling.io>
2026-07-15 15:07:36 +0200

    test: sbt-scalafix 0.14.6 and scalafix 0.14.7
    Motivation:
     * scalafix command line error
     * mainteners told us to try it
    
    Modifications:
     * use sbt-scalafix 0.14.6
     * force upgrading scalafix only 0.14.7
    
    Result:
    If reproduction => the issue is on scalafix repository
    If no reproduction => the issue is on sbt-scalafix repository

--- project/project/PluginDependencies.scala
+++ project/project/PluginDependencies.scala
@@ -7,13 +7,16 @@ object PluginDependencies {
     // -- DEPENDENCIES -- //
     // ------------------ //
     addSbtPlugin("org.scalameta"  % "sbt-scalafmt" % "2.6.1"),
-    addSbtPlugin("ch.epfl.scala"  % "sbt-scalafix" % "0.14.7"),
+    addSbtPlugin("ch.epfl.scala"  % "sbt-scalafix" % "0.14.6"),
     addSbtPlugin("com.github.sbt" % "sbt-release"  % "1.5.0"),
     addSbtPlugin("com.github.sbt" % "sbt-pgp"      % "2.3.1"),
     addSbtPlugin("com.github.sbt" % "sbt-header"   % "5.11.0"),
     addSbtPlugin("com.github.sbt" % "sbt-dynver"   % "5.1.1"),
     libraryDependencies ++= Seq(
-      "org.eclipse.jgit" % "org.eclipse.jgit" % "7.7.0.202606012155-r" // sbt-scalafix
+      "org.eclipse.jgit" % "org.eclipse.jgit" % "7.7.0.202606012155-r", // sbt-scalafix
+      // sbt-scalafix 0.14.6 pulls scalafix-interfaces 0.14.6; force it back up to 0.14.7
+      // to bisect https://github.com/scalacenter/scalafix/issues/2469 (heisenbug reported against 0.14.7)
+      "ch.epfl.scala" % "scalafix-interfaces" % "0.14.7"
     )
   )
 }
